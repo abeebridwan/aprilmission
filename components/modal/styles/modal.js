be71f0b4b6f2930f46f121bc30d3158f712071b6
Abeeb Ridwan Olumide <igortunde@yahoo.com>
2020-06-23 09:19:07 +0100

    feat: modal styles added

--- components/modal/styles/modal.js
+++ components/modal/styles/modal.js
@@ -10,28 +10,39 @@ const fadeIn = keyframes`
 `;
 
 export const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  align-content: center;
   position: fixed;
   z-index: 500;
   left: 0;
   top: 0;
   width: 100%;
   height: 100%;
-  overflow: auto;
-  background-color: rgb(0, 0, 0);
-  background-color: rgba(0, 0, 0, 0.4);
+  overflow: hidden;
   animation: ${fadeIn} 0.4s;
 `;
 
 export const Content = styled.div`
   width: 100%;
-  background-color: white;
-  height: 40%;
+  background-color: rgb(0, 0, 0);
+  background-color: rgba(0, 0, 0, 0.4);
+  height: 100%;
+  padding: 4px 30px;
 `;
 
-export const Header = styled.div``;
+export const Header = styled.div`
+  padding: 4px 100px;
+  width: 100%;
+  & svg {
+    background-color: white;
+    padding: 10px;
+    margin-left: 80%;
+    border-radius: 5px;
+  }
+`;
 
-export const Body = styled.div``;
+export const Body = styled.div`
+  width: 60%;
+  height: 83%;
+  margin: 30px auto 0;
+  background-color: white;
+  border-radius: 5px;
+`;
--- containers/Modal.js
+++ containers/Modal.js
@@ -1,12 +1,16 @@
 /* eslint-disable react/prop-types */
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import Modal from '../components/modal/index';
 
 export default function ModalContainer({ children }) {
   return (
     <Modal>
       <Modal.Content>
-        <Modal.Header>we are good</Modal.Header>
+        <Modal.Header>
+          <FontAwesomeIcon icon={faTimes} size="lg" />
+        </Modal.Header>
         <Modal.Body>I am fine</Modal.Body>
       </Modal.Content>
       {children}
