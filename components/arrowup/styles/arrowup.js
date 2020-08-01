557f3c87019193bb2e7678bdbeaf01f88e77e610
Abeeb Ridwan Olumide <igortunde@yahoo.com>
2020-08-01 08:17:12 +0100

    feat: some styles added to arrowup component

--- components/arrowup/styles/arrowup.js
+++ components/arrowup/styles/arrowup.js
@@ -1,3 +1,21 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.button`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  bottom: 20px;
+  right: 30px;
+  z-index: 999999;
+  font-weight: 700;
+  font-size: 13px;
+  border: none;
+  outline: none;
+  background-color: #f67d4a;
+  color: #c3bfbf;
+  cursor: pointer;
+  padding: 14px 18px;
+  border-radius: 60%;
+`;
--- containers/Arrowup.js
+++ containers/Arrowup.js
@@ -1,9 +1,13 @@
 /* eslint-disable react/prop-types */
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLongArrowAltUp } from '@fortawesome/free-solid-svg-icons';
+
 import ArrowUp from '../components/arrowup/index';
 
 const ArrowUpContainer = ({ children }) => (
   <ArrowUp>
-    I am gonna be the greatest
+    <FontAwesomeIcon icon={faLongArrowAltUp} size="lg" />
+    Top
     {children}
   </ArrowUp>
 );
