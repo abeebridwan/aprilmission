cf90e8c15a1a8cf570c8a8f2de9247584dd9cf0a
Abeeb Ridwan Olumide <igortunde@yahoo.com>
2020-08-06 23:22:14 +0100

    feat: _app added to prevent fontawesome bug

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+/* eslint-disable react/prop-types */
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+// Import the CSS
+config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
